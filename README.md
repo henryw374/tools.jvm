f999119a62c102b902a210c939bbe034eac2c8db
Henry Widd <henryw374@gmail.com>
2021-06-30 12:29:41 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # tools.jvm
+
 clojure tools for getting information about the jvm runtime
+
+# Usage 
+
+```clojure
+
+(require '[com.widdindustries.tools.jvm :as jvm])
+
+;get data about memory usage, classloading (and not yet... gc, threads)
+(jvm/all-snapshots)
+
+; thread dump as data
+(jvm/thread-dump)
+
+```
+
+
+# Release
+
+create a git tag.
+
+`make install VERSION=your-tag` (this installs in ~/.m2 - check that things look ok)
+
+`make deploy VERSION=your-tag`  - you need to have set up clojars credentials as per https://github.com/applied-science/deps-library
+
+`git push origin new-tag-name`
---
+++ src/com/widdindustries/tools/jvm.clj
@@ -0,0 +1,64 @@
+(ns com.widdindustries.tools.jvm
+  (:require [clojure.stacktrace :as stck])
+  (:import (java.lang.management ManagementFactory 
+                                 ClassLoadingMXBean 
+                                 BufferPoolMXBean 
+                                 MemoryType 
+                                 MemoryPoolMXBean 
+                                 MemoryUsage 
+                                 ThreadMXBean 
+                                 ThreadInfo)))
+
+(set! *warn-on-reflection* true)
+
+(defn class-loading-snapshot []
+  (let [^ClassLoadingMXBean b (ManagementFactory/getClassLoadingMXBean)]
+    {"jvm.classes.loaded" (.getLoadedClassCount b)
+     "jvm.classes.unloaded" (.getUnloadedClassCount b)}))
+
+(defn memory-snapshot []
+  {"buffer"
+   (->> (ManagementFactory/getPlatformMXBeans BufferPoolMXBean)
+        (mapv (fn [^BufferPoolMXBean b]
+                {"id"                              (.getName b)
+                 "jvm.buffer.count"                (.getCount b)
+                 "jvm.buffer.memory.used"    (.getMemoryUsed b)
+                 "jvm.buffer.total.capacity" (.getTotalCapacity b)}
+                )))
+   "memory"
+   (->> (ManagementFactory/getPlatformMXBeans MemoryPoolMXBean)
+        (mapv (fn [^MemoryPoolMXBean b]
+                (let [mem-type (if (= MemoryType/HEAP (.getType b)) "heap" "nonheap")
+                      ^MemoryUsage usage (.getUsage b)]
+                  {"id"   (.getName b)
+                   "area" mem-type
+                   "jvm.memory.init"  (.getInit usage)
+                   "jvm.memory.used"  (.getUsed usage)
+                   "jvm.memory.committed"  (.getCommitted usage)
+                   "jvm.memory.max" (.getMax usage)}))))})
+
+(defn all-snapshots []
+  {"classsloading" (class-loading-snapshot)
+   "memory" (memory-snapshot)})
+
+(defn thread-dump
+  ([] (thread-dump 100))
+  ([max-depth]
+   (let [^ThreadMXBean threadMXBean (ManagementFactory/getThreadMXBean)
+         info (.getThreadInfo threadMXBean (.getAllThreadIds threadMXBean) ^int max-depth)]
+     (for [^ThreadInfo threadInfo info]
+       {"thread.name"  (.getThreadName threadInfo)
+        "thread.state" (str (.getThreadState threadInfo))
+        "thread.stack" (let [st (.getStackTrace threadInfo)]
+                         (if-not (first st)
+                           "[empty stack trace]"
+                           (map (fn [ele]
+                                  (with-out-str
+                                    (stck/print-trace-element ele)))
+                             st)))}))))
+
+
+(comment 
+  (all-snapshots)
+  (thread-dump)
+  )
